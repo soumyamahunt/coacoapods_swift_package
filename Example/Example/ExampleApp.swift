3d0cc4086b5e344fd13ea5e6d6ee337e258c7fe8
Soumya Mahunt <soumya.mahunt@tatadigital.com>
2022-02-13 18:21:09 +0530

    add sample project

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Soumya Ranjan Mahunt on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
